3eb161b4d800940fa487e77dbbe37af9f193ecbe
Jaegeuk Kim <jaegeuk@kernel.org>
2017-04-11 19:15:33 -0700

    f2fs: give time to flush dirty pages for checkpoint
    If all the threads are waiting for checkpoint, we have no chance to flush
    required dirty pages.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -974,6 +974,7 @@ retry_flush_dents:
 		err = sync_dirty_inodes(sbi, DIR_INODE);
 		if (err)
 			goto out;
+		cond_resched();
 		goto retry_flush_dents;
 	}
 
@@ -989,6 +990,7 @@ retry_flush_dents:
 		err = f2fs_sync_inode_meta(sbi);
 		if (err)
 			goto out;
+		cond_resched();
 		goto retry_flush_dents;
 	}
 
@@ -1003,6 +1005,7 @@ retry_flush_nodes:
 			f2fs_unlock_all(sbi);
 			goto out;
 		}
+		cond_resched();
 		goto retry_flush_nodes;
 	}
 
