8129cf9df7ab3bf3f3c5c38350ce49d331e9e8de
Blagovest Kolenichev <bkolenichev@codeaurora.org>
2018-09-18 02:56:09 -0700

    Revert "UPSTREAM: ANDROID: binder: remove 32-bit binder interface."
    This reverts commit 61a775a14bd860aa6ef226284ae9affb785a4628.
    
    Removal of 32-bit binder interface is mandated from android P platform
    onwards. Many targets on msm-4.9 mainline are not using android P
    platform, so keep 64-bit binder interface as default for 32-bit and
    64-bit targets. For targets requiring 32-bit binder interface, it can
    be enabled through respective defconfig.
    
    Change-Id: I16cd053c90a811f9830ae6efec40a2650a10df9b
    Signed-off-by: Blagovest Kolenichev <bkolenichev@codeaurora.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -74,6 +74,11 @@
 #include <linux/ratelimit.h>
 
 #include "binder.h"
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
+#define BINDER_IPC_32BIT 1
+#endif
+
+#include <uapi/linux/android/binder.h>
 #include "binder_alloc.h"
 #include "binder_trace.h"
 
